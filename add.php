34d8e9f807c3407b9d154574af94671a49c48941
Fajar-Putra-Setiawan <73048332+Fajar-Putra-Setiawan@users.noreply.github.com>
2021-12-02 15:27:01 +0700

    Update add.php

--- add.php
+++ add.php
@@ -6,42 +6,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/pure-min.css" integrity="sha384-Uu6IeWbM+gzNVXJcM9XV3SohHtmWE+3VGi496jvgX1jyvDTXfdK+rfZc8C1Aehk5" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
-    <title></title>
+    <title>Add Data</title>
   </head>
   <body>
   <div class="banner">
-        <h1 class="banner-head">LIST OF INVENTORY </br> EVERYTHING OFFICE</h1>
+        <h1 class="banner-head">Add Data <br> Coffee & Beverages</h1>
     </div>
     <div class="pure-menu pure-menu-horizontal">
     <ul class="pure-menu-list">
         <li class="pure-menu-item pure-menu-selected">
             <a href="main.php" class="pure-menu-link">Home</a>
         </li>
-        <li class="pure-menu-item pure-menu-selected">
-            <a href="list product.php" class="pure-menu-link">Inventory List</a>
-        </li>
         <li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
             <a href="#" id="menuLink1" class="pure-menu-link">List per Category</a>
             <ul class="pure-menu-children">
                 <li class="pure-menu-item">
-                    <a href="#" class="pure-menu-link">Buildings</a>
+                    <a href="drinkdashboard.php" class="pure-menu-link">Drink</a>
                 </li>
                 <li class="pure-menu-item">
-                    <a href="#" class="pure-menu-link">Vehicles</a>
-                </li>
-                <li class="pure-menu-item">
-                    <a href="#" class="pure-menu-link">Office Inventory</a>
-                </li>
-                <li class="pure-menu-item">
-                    <a href="#" class="pure-menu-link">Electronic</a>
+                    <a href="dashboardsnack.php" class="pure-menu-link">Snack</a>
                 </li>
             </ul>
         </li>
-        <li class="pure-menu-item">
-            <a href="logout.php" class="pure-menu-link">Logout</a>
-        </li>
+        </div>
+        <a class="nav-link" href="logout.php"><button type="button" class="btn" style="float: right; bottom:0px; color: white; background-color: #c68b59;">Log out</button></a>
+        </div>
     </ul>
 </div>
+
+<div class="container-xxl">
+        <br><br>
+        <nav class="navbar navbar-dark" style="background-color: #c68b59; margin-bottom: 25px;">
+          <div class="container-fluid">
+            <span class="navbar-brand mb-0 a">Add Data</span>
+          </div>
+        </nav>
+
   <form method="POST" action="addcheck.php">
           <table>
             <tr>
@@ -79,17 +79,15 @@
             <tr>
               <td>
                 <br>
-                <input class="btn btn-dark" type="submit" name="submit" value="Simpan" style="color: white; background-color: #1F1D36;">
+                <input class="btn btn-dark" type="submit" name="submit" value="Save" style="color: white; background-color: #c68b59;">
                 <input class="btn btn-outline-dark" type="reset" value="Reset">
               </td>
             </tr>
           </table>
         </form>
-
-<div class="footer l-box">
-    <p>
-        <h3> Inventory Web 2021 </h3>
-    </p>
+</div>
+<div class="footer" style="margin-top: 50px;">
+    <h3>dream café</h3>
 </div>
   </body>
 </html>
